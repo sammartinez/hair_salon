e924306c64023c1250397f43ab6f6de45c8e37c9
Sam Martinez <sammartinez19@gmail.com>
2015-08-21 12:47:38 -0700

    add getStylistId test and pass

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -41,6 +41,27 @@
                 //Assert
                 $this->assertEquals(true, is_numeric($result));
             }
+
+            function test_getStylistId()
+            {
+                //Arrange
+                $name = "Becky";
+                $id = null;
+                $test_stylist = new Stylist($name, $id);
+                $test_stylist->save();
+
+                $client = "Jim";
+                $phone = "123-456-7890";
+                $stylist_id = $test_stylist->getId();
+                $test_stylist = new Client($client, $phone, $stylist_id, $id);
+                $test_stylist->save();
+
+                //Act
+                $result = $test_stylist->getStylistId();
+
+                //Assert
+                $this->assertEquals(true, is_numeric($result));
+            }
         }
 
  ?>
