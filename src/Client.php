ae6278f13cd2598be354a6ddf228f0e493b46992
Sam Martinez <sammartinez19@gmail.com>
2015-08-21 12:11:16 -0700

    add static function find to Client.php

--- src/Client.php
+++ src/Client.php
@@ -85,6 +85,25 @@
                 }
                 return $clients;
             }
+
+            static function deleteAll()
+            {
+                $GLOBALS ['DB']->exec("DELETE FROM client;");
+            }
+
+            static function find($search_id)
+            {
+                $found_client = null;
+                $clients = Client::getAll();
+
+                foreach($clients as $client) {
+                    $client_id = $client->getId();
+                    if ($client_id == $search_id) {
+                        $found_client = $client;
+                    }
+                }
+                return $found_client;
+            }
         }
 
  ?>
